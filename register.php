4f67e2658dcea117235f217d39eb4d76415ee36e
shivanshu5416jais <shivanshu5416jaiswal@gmail.com>
2018-09-26 16:39:05 +0530

    sending mail using phpmailer

--- register.php
+++ register.php
@@ -1,4 +1,5 @@
 <?php
+ error_reporting(0);
   include_once('connection.php');
   session_start();
   if(isset($_SESSION['user']))
@@ -23,19 +24,19 @@
   	{
   		$response='Fields cant be empty';
   	}
-  	else
-  	{
-       if (preg_match("/^[a-zA-Z0-9._-]+@[a-zA-Z0-9-]+\.[a-zA-Z.]{2,5}$/", $email)) 
-       {
-       $response='INVALID EMAIL';
-       }
+  	else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $response = 'Invalid email format'; 
+    }
+    else
+    {
   	   $query="SELECT `email` FROM `users` WHERE `email`='$email'";
   		 $result=mysqli_query($conn,$query);
-  		
-  	   if(mysqli_num_rows($result) > 0)
+  		//check if email is already registered
+  	        if(mysqli_num_rows($result) > 0)
             {
                 $response = 'Email already registered';
             }
+       // if new email is being registered     
             else
             {
                 $password = md5($password);
@@ -43,9 +44,44 @@
                 $query = "INSERT INTO `users`(`name`, `email`, `password`) VALUES ('$name','$email','$password')";
                //execute the query using mysqli_connect() function which returns true on success
                 if( mysqli_query($conn, $query) )
-                {
+                {    
+                     
+require './phpm/helo.php';
+
+$mail = new PHPMailer;
+
+$mail->SMTPDebug = 4;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'shivanshu5416jaiswal@gmail.com';                 // SMTP username
+$mail->Password = 'shiv1357';                           // SMTP password
+$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 587;                                    // TCP port to connect to
+
+$mail->setFrom('shivanshu5416jaiswal@gmail.com', 'TRAVEl');
+$mail->addAddress($email);     // Add a recipient              // Name is optional
+//$mail->addReplyTo(EMAIL);
+
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Here is the subject';
+$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+if(!$mail->send()) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'Message has been sent please verify your email:';
+    echo $email;
+}
+
+
                     $correct_response = 'Registration successfull...';
-                    header("refresh:2;url=tlogin.php");
+                    // to take to login page within 2 sec
+                    //header("refresh:2;url=tlogin.php");
                 }
                 else
                 {
